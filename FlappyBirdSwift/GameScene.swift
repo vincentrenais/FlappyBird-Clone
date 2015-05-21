b1ce021d7c6b81cc74ca898f1e93568c88909ab0
Vincent Renais <vincentrenais@gmail.com>
2015-05-20 23:36:44 -0400

    First commit

--- FlappyBirdSwift/GameScene.swift
+++ FlappyBirdSwift/GameScene.swift
@@ -9,36 +9,97 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var bird = SKSpriteNode()
+    var background = SKSpriteNode()
+    var movingObjects = SKNode()
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
-        self.addChild(myLabel)
+        self.addChild(movingObjects)
+        
+        var backgroundTexture = SKTexture(imageNamed: "img/bg.png")
+        var moveBackground = SKAction.moveByX(-backgroundTexture.size().width, y: 0, duration: 9)
+        var replaceBackground = SKAction.moveByX(backgroundTexture.size().width, y: 0, duration: 0)
+        var moveBackgroundForever = SKAction.repeatActionForever(SKAction.sequence([moveBackground, replaceBackground]))
+        for var i:CGFloat = 0; i < 3; i++ {
+            background = SKSpriteNode(texture: backgroundTexture)
+            background.position = CGPoint(x: backgroundTexture.size().width/2 + backgroundTexture.size().width * i, y: CGRectGetMidY(self.frame))
+            background.size.height = self.frame.height
+            background.runAction(moveBackgroundForever)
+            movingObjects.addChild(background)
+        }
+        
+        
+        var birdTexture1 = SKTexture(imageNamed: "img/flappy1.png")
+        var birdTexture2 = SKTexture(imageNamed: "img/flappy2.png")
+        var animation = SKAction.animateWithTextures([birdTexture1, birdTexture2], timePerFrame: 0.1)
+        var makeBirdFlap = SKAction.repeatActionForever(animation)
+        bird = SKSpriteNode(texture: birdTexture1)
+        bird.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
+        bird.runAction(makeBirdFlap)
+        bird.physicsBody = SKPhysicsBody(circleOfRadius: bird.size.height/2)
+        bird.physicsBody?.dynamic = true
+        bird.physicsBody?.allowsRotation = false
+        bird.zPosition = 10
+        self.addChild(bird)
+        
+        
+        
+        var ground = SKNode()
+        ground.position = CGPointMake(0,0)
+        ground.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(self.frame.size.width, 1))
+        ground.physicsBody?.dynamic = false;
+        self.addChild(ground)
+        var timer = NSTimer.scheduledTimerWithTimeInterval(3, target: self, selector: Selector("makePipes"), userInfo: nil, repeats: true)
     }
     
-    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+    func makePipes() {
+        
+        let gapHeight = bird.size.height * 4.3
+        var movementAmount = arc4random() % UInt32(self.frame.size.height/2)
+        var pipeOffSet = CGFloat(movementAmount) - self.frame.size.height / 4
+        var movePipes = SKAction.moveByX(-self.frame.size.width * 2, y: 0, duration: NSTimeInterval(self.frame.size.width / 100))
+        var removePipe = SKAction.removeFromParent()
+        var moveAndRemove = SKAction.repeatActionForever(SKAction.sequence([movePipes, removePipe]))
+        
+        // Pipe 1
+        
+        var pipeTexture1 = SKTexture(imageNamed: "img/pipe1.png")
+        var pipe1 = SKSpriteNode(texture: pipeTexture1)
+        pipe1.runAction(moveAndRemove)
+        pipe1.physicsBody = SKPhysicsBody(rectangleOfSize: pipe1.size)
+        pipe1.physicsBody?.dynamic = false
+        pipe1.position = CGPoint(x: CGRectGetMidX(self.frame) + self.frame.size.width, y: CGRectGetMidY(self.frame) + pipe1.size.height/2 + gapHeight/2 + pipeOffSet)
+        movingObjects.addChild(pipe1)
+        
+        // Pipe 2
+        
+        var pipeTexture2 = SKTexture(imageNamed: "img/pipe2.png")
+        var pipe2 = SKSpriteNode(texture: pipeTexture2)
+        pipe2.runAction(moveAndRemove)
+        pipe2.physicsBody = SKPhysicsBody(rectangleOfSize: pipe2.size)
+        pipe2.physicsBody?.dynamic = false
+        pipe2.position = CGPoint(x: CGRectGetMidX(self.frame) + self.frame.size.width, y: CGRectGetMidY(self.frame) - pipe2.size.height/2 - gapHeight/2 + pipeOffSet)
+        movingObjects.addChild(pipe2)
+        
+        
+        var gap = SKNode()
+        gap.position = CGPoint(x: CGRectGetMidX(self.frame) + self.frame.size.width, y: CGRectGetMidY(self.frame) + pipeOffSet)
+        gap.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(pipe1.size.width, gapHeight))
+        gap.runAction(moveAndRemove)
+        gap.physicsBody?.dynamic = false
+        movingObjects.addChild(gap)
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent){
         /* Called when a touch begins */
         
-        for touch in (touches as! Set<UITouch>) {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+        bird.physicsBody?.velocity = CGVectorMake(0, 0)
+        bird.physicsBody?.applyImpulse(CGVectorMake(0, 50))
+        
     }
-   
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
